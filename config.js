672d38f5a9c2259f56b153266b713e4c757b2003
duong1998nd <duong98nd98@gmail.com>
2023-07-21 12:10:03 +0700

    first commit

--- config.js
+++ config.js
@@ -2,13 +2,13 @@
 var config = {
     // 下面的句子不一定非要7句， 你也可以改成4句，2句话都可以
     lines: [
-        "Chúc",
-        "Em ngủ ngon",
-        "Hôm nay về tiếng việt",
-        "Cho khỏi sai ngữ pháp",
-        "Ngại ngùng é",
-        "Hihi",
-        "duong。。。",
+        "Đây chưa hẳn là công việc của e",
+        "Thực ra e muốn hướng tới thứ khác cơ",
+        "Có thể nói là em đang đi học part-time :v",
+        "Làm part-time",
+        "Nhưng mà chị đã hỏi mỗi công việc, nên là",
+        "E làm những thứ ngu ngốc như này đó",
+        "^^",
     ],
     // 相爱的时间 记住格式不能写错了, 非常重要
     // 格式一定要和下面这个时间格式一样
@@ -18,5 +18,5 @@ var config = {
     //          2.T不能丢了
     date: "2023-06-20T20:15:00",
     // 双方的名字
-    names: ["xiao", "keai!"]
+    names: ["xiao", "huỳn!"]
 }
